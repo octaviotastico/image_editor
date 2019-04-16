5f371c6036b04e4f26bb3c9140b283518d63282d
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-04-16 01:40:09 -0300

    Minor changes in main, just for testing

--- app/App.js
+++ app/App.js
@@ -9,12 +9,31 @@ import { Platform,
 class App extends React.Component {
   render() {
     return (
-      <ImageBackground></ImageBackground>
+      <ImageBackground
+        source={require('./background.png')}
+        style={styles.container}>
+
+        <View style={styles.overlayContainer}>
+
+
+        </View>
+
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  }
+
+  overlayContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(47, 163, 218, .4)'
+  }
 
 });
 
