ae00ab6683adf0ec83afabcecc44708db708caa6
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-04-17 00:26:09 -0300

    Added simple menu with icons

--- app/App.js
+++ app/App.js
@@ -5,6 +5,7 @@ import { Platform,
          View,
          ImageBackground
         } from 'react-native';
+import MenuItem from './components/MenuItems';
 
 class App extends React.Component {
   render() {
@@ -13,8 +14,16 @@ class App extends React.Component {
         source={require('./background.png')}
         style={styles.container}>
 
-        <View style={styles.overlayContainer}>
+        <View style={styles.bigView}>
 
+          <View style={styles.topView}>
+            <Text style={styles.mainTitle}>H O M E</Text>
+          </View>
+
+          <View style={styles.menuView}>
+            <MenuItem itemImage={require('./icons/camera.png')} />
+            <MenuItem itemImage={require('./icons/galery.png')} />
+          </View>
 
         </View>
 
@@ -27,13 +36,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    height: '100%',
-  }
+    height: '100%'
+  },
 
-  overlayContainer: {
+  bigView: {
     flex: 1,
     backgroundColor: 'rgba(47, 163, 218, .4)'
-  }
+  },
+
+  mainTitle: {
+    color: '#fff',
+    borderColor: '#fff',
+    borderWidth: 2,
+    padding: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+    fontSize: 28,
+    backgroundColor: 'rgba(255, 255, 255, .1)'
+  },
+
+  topView: {
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  menuView: {
+    height: '40%',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
 
 });
 
